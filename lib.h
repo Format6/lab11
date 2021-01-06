065d79c952526a99fa514ffd83d7341a24149325
Format6 <dmitryr0manenk01248@gmail.com>
2021-01-06 17:46:49 +0200

    Create lib.h

---
+++ lib.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*  Кiлькiсть елементiв у масивi
+*/
+#define N 6
+
+/**
+* підрахунок відповідних елементів
+*/
+int* counter(int* arr);
+
+/**
+* Довжина масиву
+*/
+int size(int* arr);
